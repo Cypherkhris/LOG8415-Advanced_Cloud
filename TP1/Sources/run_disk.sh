5a5f776de0e6dbc91fd731701c1a60c4c256fe81
Mathieu Gamache <mathg99@hotmail.com>
2017-02-06 16:14:46 -0500

    Must be in sudo

--- TP1/Sources/run_disk.sh
+++ TP1/Sources/run_disk.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Base on article http://support.commgate.net/index.php?/Knowledgebase/Article/View/
 
+# Check if root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 while getopts r: option
 do
         case "${option}"
