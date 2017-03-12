6371313e7572ccf939557839896d133c2a4293f8
Mathieu <math.gamache@gmail.com>
2017-03-12 12:56:53 -0400

    Script for Q4: Number of VIP (need to recheck)
    Need to recheck because not sure of the definition of VIP (some client seem to switch between the VIP status and not (VIP count = 2486 and Not VIP count = 2489 and we have 2500 distinct client....))

---
+++ TP2/Sources/SparkQuery/q4_HowManyVipMember.py
@@ -0,0 +1,21 @@
+# Comprend un SparkSession mais aussi SparkContext que avant
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+if __name__ == "__main__": 
+
+    spark = SparkSession.builder.appName("DistincMember").getOrCreate()
+
+    # Convert data to Spark Dataset
+    salesDataset = spark.read.option("header","true").csv("hdfs:////user/maria_dev/tp-data/data_dump.csv")
+
+    # Count the number of distinc member who is VIP
+    # TODO: Is a user always VIP??? (doesn't seem so because number of not VIP is 2489)
+    count = salesDataset.where((col("vip") == "true")).select("member_id").distinct().count()
+
+    print("Distinct vip: " + str(count))
+    # Answer = 2486
+
+
+
+
