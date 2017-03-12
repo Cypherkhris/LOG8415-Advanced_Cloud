e129c6765698260d778a5aad22f1e1db2effb121
Mathieu <math.gamache@gmail.com>
2017-03-12 13:03:33 -0400

    Script for Q5: Female canadians purchaser for zone7

---
+++ TP2/Sources/SparkQuery/q5_SpecificPurchaseCount.py
@@ -0,0 +1,24 @@
+# Comprend un SparkSession mais aussi SparkContext que avant
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+if __name__ == "__main__": 
+
+    spark = SparkSession.builder.appName("DistincMember").getOrCreate()
+
+    # Convert data to Spark Dataset
+    salesDataset = spark.read.option("header","true").csv("hdfs:////user/maria_dev/tp-data/data_dump.csv")
+
+    # Count of specific purchaser
+    count = salesDataset.where(
+        (col("country") == "CA") & 
+        (col("gender") == "Female") & 
+        (col("zone") == "zone7")
+        ).select("member_id").distinct().count()
+
+    print("Distinct Canadian female members who purchased an item from Zone7: " + str(count))
+    # Answer = 16
+
+
+
+
