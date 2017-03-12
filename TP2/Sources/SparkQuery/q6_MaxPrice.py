3003e052bdb43dbdce8096c80c3939161a88d93d
Mathieu <math.gamache@gmail.com>
2017-03-12 13:13:32 -0400

    Script Q6: Max priced

---
+++ TP2/Sources/SparkQuery/q6_MaxPrice.py
@@ -0,0 +1,19 @@
+# Comprend un SparkSession mais aussi SparkContext que avant
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import desc
+
+if __name__ == "__main__": 
+
+    spark = SparkSession.builder.appName("DistincMember").getOrCreate()
+
+    # Convert data to Spark Dataset
+    salesDataset = spark.read.option("header","true").csv("hdfs:////user/maria_dev/tp-data/data_dump.csv")
+
+    # Find max price
+    orderedByPrice = salesDataset.orderBy(desc("amount")).select("product_id", "amount")
+
+    orderedByPrice.show(1)
+    # Answer: id: 330092 at 9999.87$
+
+
+
