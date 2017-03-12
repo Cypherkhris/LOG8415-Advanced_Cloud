7c28f56c335aead098697fcc758003b6640325cf
Mathieu <math.gamache@gmail.com>
2017-03-12 12:07:27 -0400

    Script for Q2: First purchase

---
+++ TP2/Sources/SparkQuery/q2_FirstPurchase.py
@@ -0,0 +1,26 @@
+# Comprend un SparkSession mais aussi SparkContext que avant
+from pyspark.sql import SparkSession
+from pyspark.sql import functions
+
+if __name__ == "__main__": 
+
+    spark = SparkSession.builder.appName("DistincMember").getOrCreate()
+
+    # Convert data to Spark Dataset
+    salesDataset = spark.read.option("header","true").csv("hdfs:////user/maria_dev/tp-data/data_dump.csv")
+
+    pattern = "yyyy-MM-dd"
+
+    # Converting to unix timestamp for ordering
+    convertedDataset = salesDataset.withColumn("timestampDate", functions.unix_timestamp("date", pattern).cast("timestamp"))
+    
+    # Order the dataset
+    sortedDate = convertedDataset.orderBy("timestampDate")
+
+    # Get the first purchase
+    sortedDate.select("date").show(1)
+    # Answer = 2007-12-02
+
+
+
+
