6497797413529cebb5a1861a72c9225e4675232f
Marie Martin <marie.martin@etu.utc.fr>
2017-03-14 15:25:25 -0400

    Modificaiton to regex

---
+++ TP2/Sources/plot_spark_benchmark.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as pyplot
+import numpy
+
+# inspired by http://people.duke.edu/~ccc14/pcfb/numpympl/MatplotlibBarPlots.html
+
+xTickMarks = ["azure A1", "azure A4", "amazon T2", "amazon C4", "amazon M4", "amazon R4"]
+N = 6
+CPU_total_time = [66.8626, 66.6122, 29.8535, 25.0010, 29.3211, 27.8841]
+CPU_avg_request = [6.69, 6.66, 2.986, 2.50, 2.93, 2.79]
+
+ind = numpy.arange(N)
+width = 0.35
+
+fig = pyplot.figure()
+fig.suptitle("sysbench CPU benchmark")
+
+ax = fig.add_subplot(121) # subplot(nbcol, nbligne, numfigure)
+ax.bar(ind, CPU_total_time, width)
+ax.set_xlim(-width,len(ind)+width)
+ax.set_ylim(min(CPU_total_time)-1, max(CPU_total_time)+1)
+ax.set_xticks(ind)
+ax.set_ylabel("total time (s)")
+xtickNames = ax.set_xticklabels(xTickMarks)
+pyplot.setp(xtickNames, rotation=45, fontsize=10)
+
+ax2 = fig.add_subplot(122)
+ax2.bar(ind, CPU_avg_request, width)
+ax2.set_xlim(-width,len(ind)+width)
+ax2.set_ylim(min(CPU_avg_request)-1, max(CPU_avg_request)+1)
+ax2.set_xticks(ind)
+ax2.set_ylabel("avg time per request (ms)")
+xtickNames = ax2.set_xticklabels(xTickMarks)
+pyplot.setp(xtickNames, rotation=45, fontsize=10)
+
+pyplot.tight_layout()
+pyplot.subplots_adjust(top=0.9)
+
+pyplot.show()
+
