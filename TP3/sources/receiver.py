06dea8f9bc191c657bf69d9058befc0899216ac6
Mathieu <math.gamache@gmail.com>
2017-04-20 11:10:02 -0400

    Basic setup

---
+++ TP3/sources/receiver.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+"""Python module that receives TCP requests."""
+
+import socket
+import ConfigParser
+from random import randint
+
+host = '127.0.0.1'
+port = 5001
+
+"""Load config"""
+modeConfig = ConfigParser.ConfigParser()
+modeConfig.readfp(open(r'mode.config'))
+mode = modeConfig.get('ProxyMode', 'mode')
+
+clusterConfig = ConfigParser.ConfigParser()
+clusterConfig.readfp(open(r'cluster.config'))
+slaveCount = clusterConfig.get('ClusterInfo', 'slaveCount')
+
+
+
+
+def main():
+    """Main."""
+    s = socket.socket()
+    s.bind(('', port))
+
+    s.listen(1)  # Listen to one connection
+    c, addr = s.accept()
+    print 'connection from: ' + str(addr)
+
+    while True:
+        data = c.recv(2048)  # Max bytes
+        if not data:
+            break
+
+        print 'from connected user: ' + str(data)
+        data = str(data)
+
+        type, command = parse_data(data)
+
+        target = get_target()
+
+        # if type == 'insert':
+        #     hit_master(command)
+        # else:
+        #   my_pattern(command)
+        #
+
+        response = str('Command handle by: ' + target)
+        c.send(response)
+
+    # c.close()
+
+
+def parse_data(data):
+    """Function that takes the data and parses it."""
+    type = 'select'
+
+    if 'INSERT' in data:
+        type = 'insert'
+
+    command = data
+
+    return type, command
+
+
+def get_target():
+    """
+        0: master
+        [1,...]: slave id
+    """
+
+    options = {
+        'direct' : direct_mode,
+        'random' : random_mode,
+        'balancing' : balancing_mode
+    }
+
+    target = options[mode]()
+    return target
+
+def direct_mode():
+    return 0
+
+def random_mode():
+    return randint(1, slaveCount)
+
+def balancing_mode():
+    return 0
+
+
+def my_pattern():
+    """Implement algorithm of the pattern here."""
+    # Connect to MySQL Cluster
+    # Hit the database based on the algorithm
+    # TO DO ...
+
+
+if __name__ == '__main__':
+    main()
--- TP3/sources/sender.py
+++ TP3/sources/sender.py
@@ -2,16 +2,26 @@
 """Python module that sends TCP requests to AWS instance."""
 
 import socket
+import ConfigParser
 from time import sleep
 
-host = 'ec2-52-89-34-120.us-west-2.compute.amazonaws.com' # DNS public du master
-port = 5001
+
 insert = 'INSERT INTO transactions VALUES ('
 select = 'SELECT * FROM transactions WHERE serial = '
 
 
+
 def main():
     """Main."""
+
+    """Load config"""
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(r'cluster.config'))
+    host = config.get('Master', 'host')
+    port = config.get('Master', 'port')
+
+
+
     s = socket.socket()
     s.connect((host, port))
 
@@ -21,7 +31,7 @@ def main():
         for line in lines:
             print line
             member_id, date, country, gender, ip_address, amount, vip, product_id, card_type, serial, zone = line.split(",")
-            if(insert):
+            if(do_insert):
                 cmd = insert + member_id + ", '" + date + "', '" + country + "', '" + gender + "', '" + ip_address + "', " + amount + ", " + vip + ", " + product_id + ", '" + card_type + "', '" + serial + "', '" + zone + "');"
                 do_insert = False;
             else:
