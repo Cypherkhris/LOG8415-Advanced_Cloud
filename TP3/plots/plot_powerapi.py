74df4e7a8ff2a91b23e9b0013ad14edcaaae79a1
Marie Martin <marie.martin@etu.utc.fr>
2017-04-30 13:27:13 -0400

    Update plot for Joules unit

--- TP3/plots/plot_powerapi.py
+++ TP3/plots/plot_powerapi.py
@@ -12,17 +12,22 @@ def main():
 
     with open(filename, 'r') as f:
         lines = f.readlines()
+        seconds = 0.5
         for line in lines:
             line = line.strip('\n')
             id, timestamp, value = line.split(';')
+            value = float(value) * 0.001 * seconds # calcul en joules : W * s avec W = 0.001 * mW
             values.append(value)
             timestamps.append(timestamp)
+            seconds += 0.5
 
     fig = pyplot.figure()
     fig.suptitle(plotname)
 
     ax = fig.add_subplot(111) # subplot(nbcol, nbligne, numfigure)
     ax.plot(timestamps, values)
+    ax.set_xlabel('Timestamp')
+    ax.set_ylabel('Consommation (J)')
     pyplot.tight_layout()
     pyplot.subplots_adjust(top=0.9)
 
